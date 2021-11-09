d8c4cb005b978e6e5ada2f02e7c343a9c1b8d7a6
mef-lang <66832069+mef-lang@users.noreply.github.com>
2021-11-09 11:20:28 +0100

    mise en place de l'operateur de comparaison

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 // les variables
 const value = document.querySelector(".value");
+// récuperation de tous les boutons
 const boutons = document.querySelectorAll("button");
-const count = 0;
+// demarrage du compteur
+let count = 0;
 
-
+// récuperation d'un bouton parmi la liste des boutons
 boutons.forEach(function (btn){
+    // capture de l'evenement au clic
     btn.addEventListener("click", function (e){
+
         let style = e.currentTarget;
-        console.log(style.id);
+
+        if (style.id === "add"){
+            count++;
+        }else if(style.id === "remove"){
+            count--;
+        }else {
+           count = 0;
+        }
+        value.innerHTML = count
     })
 })
 
